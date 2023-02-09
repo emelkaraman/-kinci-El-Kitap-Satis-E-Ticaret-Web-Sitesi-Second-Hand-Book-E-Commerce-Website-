35477f77bfd843e509c5ec7eb1c6ec67eddcc679
emelkaraman <91911554+emelkaraman@users.noreply.github.com>
2023-02-09 17:45:45 +0300

    Add files via upload

---
+++ İkinciElKitapSatisVol1/AppClass/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using İkinciElKitapSatisVol1.Models;
+
+namespace İkinciElKitapSatisVol1.AppClass
+{
+    public class Item
+    {
+        public Kitap kitaplar { get; set; }
+        public int adet { get; set; }
+        public decimal tutar { get; set; }
+        
+    }
+}
